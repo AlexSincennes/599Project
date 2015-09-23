f653a779ec3e887cfefe7e3141bae476cafc8957
crmxm <crmcrm1992@gmail.com>
2015-09-23 15:02:48 -0700

    Level Changed

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,7 +6,6 @@ using System.Collections;
 public class Door : MonoBehaviour {
 
     public GameObject obj;
-	public GameObject obj1;
     // Use this for initialization
     void Start ()
     {
@@ -16,7 +15,6 @@ public class Door : MonoBehaviour {
     void OnTriggerEnter(Collider other)
     {
         obj.SetActive(false);
-		obj1.SetActive(false);
     }
 	
 	// Update is called once per frame
