626e58176a4b7fddd34e0bcec2b5ad98337bb5ff
LiuUS <liu462@usc.edu>
2015-10-14 16:12:00 -0700

    Add bgcamera & level compeleted to wblevel

---
+++ Assets/VictoryW.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryW : MonoBehaviour {
+	public GameObject passLabel;
+
+
+	// Use this for initialization
+	void Start () {
+		passLabel.SetActive (false);
+
+	}
+	
+	//// Update is called once per frame
+	void Update () {
+
+		
+		}
+
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+
+
+		GameManager.Instance.player.GetComponent<RaycastCharacterController> ().controllerActive = false;
+
+		passLabel.SetActive (true);
+
+	}
+}
