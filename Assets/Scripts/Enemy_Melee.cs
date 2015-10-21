05350e768e367379762b98cae25065c27e8effd5
LiuUS <liu462@usc.edu>
2015-10-20 20:09:48 -0700

    Bug fixed

--- Assets/Scripts/Enemy_Melee.cs
+++ Assets/Scripts/Enemy_Melee.cs
@@ -7,41 +7,43 @@ public class Enemy_Melee : MonoBehaviour {
 	public const int IDLE = 3;
 	public const int GETHIT = 4;
 	public const int FOLLOW = 5;
-
+	
 	public float attackDis;
 	public float patrolDis;
 	public float followDis;
-
+	
 	public Vector3 targetPosition;
 	public Vector3 startPosition;
-
+	
 	public float curSpeed = 3;
 	public float RotateSpeed = 20;
-
+	
 	public float attackRate;
 	public bool moveLeft = true;
-
+	
+	
 	private bool canAttack = false;
+	private bool playerJumpOver = false;
 	private Transform player = null;
 	private int state = WALK;
 	private float lastAttackTime = 0;
-
+	
 	private Animation myAni;
-
+	
 	void Start () {
 		if(player == null) {
 			player = GameManager.Instance.player.transform;
 		}
-
+		
 		myAni = transform.GetComponentInChildren<Animation> ();
 	}
-
+	
 	void Update () {
 		
 		switch (state) {
 		case WALK:
 			myAni.CrossFade("run",0.1f);
-
+			playerJumpOver = false;
 			if(moveLeft)
 			{//
 				if(transform.position.x - startPosition.x + patrolDis <= 0.01f || transform.position.x - startPosition.x + followDis <= 0.01f)
@@ -63,6 +65,17 @@ public class Enemy_Melee : MonoBehaviour {
 			}
 			break;
 		case ATTACK:
+			//Debug.Log("transform..x "+transform.position.x+"player..x "+player.position.x+"moveLeft "+moveLeft);
+			if(transform.position.x > player.position.x && !moveLeft ) // left
+			{
+				
+				playerJumpOver = true;
+			}else if(transform.position.x < player.position.x && moveLeft )
+			{
+				playerJumpOver = true;
+			}
+			
+			
 			float dis_attack1 = Vector3.Distance(transform.position,player.position);
 			if(Time.time - lastAttackTime > attackRate)
 			{
@@ -88,6 +101,7 @@ public class Enemy_Melee : MonoBehaviour {
 		case FOLLOW:
 			myAni.CrossFade("run",0.1f);
 			float dis = Mathf.Abs(transform.position.x - player.position.x);
+			
 			if(dis - attackDis <= 0.01f)
 			{
 				SetState(ATTACK);
@@ -101,12 +115,16 @@ public class Enemy_Melee : MonoBehaviour {
 				{
 					targetPosition = new Vector3(player.position.x, transform.position.y ,0);	
 				}
-					
+				
 			}
 			break;
 		}
-
-
+		
+		if (playerJumpOver) 
+		{
+			moveLeft = !moveLeft;
+		}
+		
 		Vector3 targetRotation = Vector3.zero;
 		if (moveLeft ) { //&& state != ATTACK&& state != IDLE
 			targetRotation = new Vector3 (0, 180, 0);
@@ -115,10 +133,10 @@ public class Enemy_Melee : MonoBehaviour {
 			targetRotation = new Vector3 (0, 0, 0);
 			transform.eulerAngles = Vector3.Lerp (this.transform.eulerAngles, targetRotation, Time.deltaTime * RotateSpeed);
 		}
-
+		
 		transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * curSpeed);
 	}
-
+	
 	public void SetState(int state) 
 	{
 		if (this.state == state)
@@ -142,7 +160,7 @@ public class Enemy_Melee : MonoBehaviour {
 			break;
 		}
 	}
-
-
+	
+	
 	
 }
