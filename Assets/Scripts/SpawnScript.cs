ab718b5626545c544bcfe4c88bdf97eeec7740d7
DowncastHarp <msnlietzke@yahoo.com>
2015-11-02 02:45:06 -0600

    Added a Few New LevelPrefabs
    -Renamed the "LevelBlocks" directory to "LevelPrefabs"
    -Created a WallJump prefab that can transition the player to a higher
    level.
    -Created a few aesthetically different, but functionally the same
    downward transition prefabs that could potentially be used to transition
    the player to a lower level.
    -Added an x- and y- Offset variable to the spawn script that can be used
    to spawn blocks on the upper and lower levels as the player approaches a
    transition. (The xOffset was always there, it's just now clear that it
    only affects the x-axis)

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -12,7 +12,8 @@ public class SpawnScript : MonoBehaviour {
 	private float spawnDist = 0f;	//Distance to travel until spawning object
 	private float genDist = 0f;		//Distance to travel until generating new object
 	private float travelDist = 0f;	//Distance traveled since last reset
-	private float offset;
+	private float xOffset;
+	private float yOffset = 0f;
 	private Vector3 lastPosition;
 
 	// Use this for initialization
@@ -29,26 +30,26 @@ public class SpawnScript : MonoBehaviour {
 		//If the last thing that happened was spawning 
 		if (spawned) {
 			if(travelDist >= genDist){
-				offset = travelDist - genDist;
+				xOffset = genDist - travelDist;
 				objIndex = RandFuncs.Sample (weights);
 				spawnDist = obj[objIndex].GetComponent<PrefabLength>().Length / 2.0f;
 				spawned = false;
-				travelDist = offset;
+				travelDist = -xOffset;
 			}
 		} else {
 			//Spawn a level block after distance traveled >= spawnDistance;
 			if (travelDist >= spawnDist) {
-				offset = travelDist - spawnDist;
-				Spawn (offset);
+				xOffset = spawnDist - travelDist;
+				Spawn (xOffset);
 				spawned = true;
-				travelDist = offset;
+				travelDist = -xOffset;
 			}
 		}
 		lastPosition = transform.position;
 	}
 
-	void Spawn(float offset){
-		Vector3 spawnPos = transform.position - new Vector3(offset, 0, 0);
+	void Spawn(float xOffset){
+		Vector3 spawnPos = transform.position + new Vector3(xOffset, yOffset, 0);
 		Instantiate(obj[objIndex], spawnPos, Quaternion.identity);
 		//Set the genDist so that a new object is "generated" after traveling half the length of the spawned prefab
 		genDist = obj[objIndex].GetComponent<PrefabLength>().Length / 2.0f;
