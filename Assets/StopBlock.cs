8d964a7d7138c58b9d0b074fa48948b991ce70bf
DowncastHarp <msnlietzke@yahoo.com>
2015-11-04 20:08:33 -0600

    Fixed StopBlock

--- Assets/StopBlock.cs
+++ Assets/StopBlock.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class StopBlock : MonoBehaviour {
 	public Sprite boxEmpty;
-	private GameObject obj;
+	private RaycastCharacterController2D obj;
+	private GhostMovement ghost;
 	public float delay;
 	public float SlowSpeed;
 	private float Speed;
 	private int temp;
 	private float t;
 	private float r;
+	private float storedSpeed;
 	// Use this for initialization
 	void Start () {
-
+		obj = null;
+		temp = 0;
 	}
 	
 	// Update is called once per frame
@@ -21,8 +24,9 @@ public class StopBlock : MonoBehaviour {
 			t = Time.time;
 			r = t + delay;
 			if (obj != null) {
-				obj.GetComponentInParent<RaycastCharacterController2D>().movement.walkSpeed = SlowSpeed;
-				obj.GetComponentInParent<RaycastCharacterController2D>().movement.runSpeed = SlowSpeed;
+				obj.movement.walkSpeed = SlowSpeed;
+				obj.movement.runSpeed = SlowSpeed;
+				ghost.playerSlowed = true;
 				temp = 2;
 			}
 		} else if (temp == 2) {
@@ -31,8 +35,9 @@ public class StopBlock : MonoBehaviour {
 			}
 			if(Time.time > r){
 				//Destroy(this.gameObject);
-				obj.GetComponentInParent<RaycastCharacterController2D>().movement.walkSpeed = 10;
-				obj.GetComponentInParent<RaycastCharacterController2D>().movement.runSpeed = 10;
+				ghost.playerSlowed = false;
+				obj.movement.walkSpeed = storedSpeed;
+				obj.movement.runSpeed = storedSpeed;
 				temp = 0;
 			}
 		}
@@ -40,9 +45,12 @@ public class StopBlock : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Player") {
 			temp = 1;
-			obj = other.gameObject;
+			obj = other.gameObject.GetComponentInParent<RaycastCharacterController2D>();
 			Destroy (this.GetComponent<BoxCollider2D> ());
 			this.GetComponent<SpriteRenderer>().sprite = boxEmpty;
+
+			storedSpeed = obj.movement.walkSpeed;
 		}
+		ghost = GameObject.FindGameObjectWithTag ("Ghost").GetComponent<GhostMovement>();
 	}
 }
