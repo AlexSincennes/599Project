2f010e9dcd47f340d0ac2b3661cd9e898a1e7743
LiuUS <liu462@usc.edu>
2015-11-17 23:06:08 -0800

    ExclamationMark update

---
+++ Assets/Scripts/MobileControl/AlertMark2D.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlertMark2D : MonoBehaviour {
+
+	public GameObject alertSprite;
+
+	void Start()
+	{
+		alertSprite.SetActive(false);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			alertSprite.SetActive(true);
+		}
+	}
+
+
+}
