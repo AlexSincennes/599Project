98375cb0c30b4599f1ff949d720317d8bed6a4e5
LiuUS <liu462@usc.edu>
2015-12-02 15:21:05 -0800

    new scripts for AlertTips

---
+++ Assets/ShowTipPattern.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowTipPattern : MonoBehaviour {
+	TipPatternManager tpm;
+	// Use this for initialization
+	void Start () {
+		tpm = GameObject.Find("UI Root").GetComponentInChildren<TipPatternManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			//manager
+			tpm.setTrue(1);
+		}
+	}
+
+
+}
