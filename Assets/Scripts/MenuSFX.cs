20415f3ec28de089312d931dd39aeb44ca8d2031
Timothy So <sot@usc.edu>
2015-12-09 14:00:59 -0800

    Added button SFX

---
+++ Assets/Scripts/MenuSFX.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSFX : MonoBehaviour {
+
+	public AudioClip clickSound;
+	
+	private AudioSource source;
+
+	// Use this for initialization
+	void Start ()
+	{
+		source = GetComponent<AudioSource>();
+	}
+	
+	public void OnClickButton()
+	{
+		if (source)
+		{
+			source.PlayOneShot(clickSound);
+		}
+	}
+}
